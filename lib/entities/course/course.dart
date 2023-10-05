b72aa905fb954adcbac860c0a9b9dd74c81f5235
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-10-05 00:38:21 -0700

    completed course entity

--- lib/entities/course/course.dart
+++ lib/entities/course/course.dart
@@ -1,3 +1,5 @@
+import 'package:course_management_app/entities/student/student.dart';
+import 'package:course_management_app/entities/teacher/teacher.dart';
 import 'package:isar/isar.dart';
 
 part 'course.g.dart';
@@ -7,4 +9,8 @@ class Course{
   // ایدی هر دوره به صورت افزایشی تولید میشه
   Id id = Isar.autoIncrement;
   late String title;
+  @Backlink(to: "course")
+  final teacher = IsarLink<Teacher>();
+  @Backlink(to: "courses")
+  final students = IsarLinks<Student>();
 }
