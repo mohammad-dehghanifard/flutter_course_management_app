e88d673023072d74b192ff0608e87ba5574f4a64
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-10-05 00:55:02 -0700

    impl openDb method in IsarService class

--- lib/service/isar_service.dart
+++ lib/service/isar_service.dart
@@ -1,3 +1,27 @@
-class IsarService{
+import 'package:course_management_app/entities/course/course.dart';
+import 'package:course_management_app/entities/student/student.dart';
+import 'package:course_management_app/entities/teacher/teacher.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
+class IsarService{
+  late Future<Isar> db;
+
+  IsarService(){
+    db = openDb();
+  }
+
+
+  Future<Isar> openDb() async {
+    // تنها در صورتی که از دیتابیس نمونه ای وجود نداشته باشه، دیتابیس جدید ساخته میشه
+    if(Isar.instanceNames.isEmpty){
+      final dir = await getApplicationDocumentsDirectory();
+      return await Isar.open(
+          [CourseSchema,StudentSchema,TeacherSchema],
+        directory: dir.path,
+        inspector: true,
+      );
+    }
+    return Future.value(Isar.getInstance());
+  }
 }
